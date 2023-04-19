73dd916a3dd4578ee827f9aac238269bda9cb358
William Lavery <lavery_william@yahoo.com>
2023-04-19 19:40:46 -0400

    Add elements to arrays

---
+++ friends.js
@@ -0,0 +1,11 @@
+var arrays = ["Dan", "Scotty", "Devin", "Adrian", "KVG"];
+
+// print 3rd element
+console.log(arrays[2]);
+
+// print last element
+console.log(arrays[4]);
+
+// print 1st element
+console.log(arrays[0]);
+

7a395140f03165d8afebc5650ef89bf023e28e27
William Lavery <lavery_william@yahoo.com>
2023-04-19 19:48:32 -0400

    Add an array of strings with places to visit

--- friends.js
+++ friends.js
@@ -9,3 +9,7 @@ console.log(arrays[4]);
 // print 1st element
 console.log(arrays[0]);
 
+// define var with 5 places to visit
+var placesTovisit = ["Wildwood", "Bellingham", "Portland", "Seattle", "Denver"];
+console.log(placesTovisit.pop());
+
